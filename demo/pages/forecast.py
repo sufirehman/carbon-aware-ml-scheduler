af75fe43ba88b30235d21f89d380a734129c22e4
Dell <sufirehman12@gmail.com>
2026-05-24 12:38:35 +0100

    updated

--- demo/pages/forecast.py
+++ demo/pages/forecast.py
@@ -7,7 +7,7 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-from core.carbon_api import CarbonAPI
+from camltc.carbon_api import CarbonAPI
 
 st.set_page_config(
     page_title="CarbonML · Forecast",
